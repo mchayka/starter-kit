c992e38a2184c01270871864043c87ff66272df8
mc <mike.chayka@gmail.com>
2013-10-09 13:59:50 +0300

    Changed folder names to fit ez publish name convention

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/style.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <link rel="stylesheet" href="stylesheets/style.css">
+        <script src="javascript/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
+        <?php include "parts/header.php"; ?>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/main.js"></script>
+        <script>window.jQuery || document.write('<script src="javascript/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="javascript/main.js"></script>
     </body>
 </html>
