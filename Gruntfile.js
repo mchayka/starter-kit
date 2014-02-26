e6a4cc7bb547a0d1541775f82e482fa85d0cde33
mc <mc@nxc.no>
2014-02-26 14:30:06 +0200

    grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,9 @@ module.exports = function (grunt) {
 
 		// Copy
 		copy: {
-			build: {
+			all: {
 				cwd: 'src',
-				src: [ 'images/**/*', 'javascript/**/*', 'favicon.ico', 'apple-touch-icon-precomposed.png' ],
+				src: [ '**/*.{png,jpg,gif,svg,ico}', 'javascript/**/*' ],
 				dest: '',
 				expand: true
 			},
@@ -15,21 +15,35 @@ module.exports = function (grunt) {
 				src: [ 'javascript/**/*' ],
 				dest: '',
 				expand: true
+			},
+			images: {
+				cwd: 'src',
+				src: [ '**/*.{png,jpg,gif,svg,ico}' ],
+				dest: '',
+				expand: true
 			}
 		},
 
 		// Clean
 		clean: {
-			build: {
-				src: [ 'javascript/**/*', 'images/**/*', 'favicon.ico',  'apple-touch-icon-precomposed.png', '*.html', 'stylesheets/**/*.css' ]
+			all: {
+				src: [ 'javascript/**/*', 'images/**/*', '*.{png,jpg,gif,svg,ico}', '*.html', 'stylesheets/**/*.css' ]
 			}
 		},
 
 		// Sass
 		sass: {
-			build: {
+			nested: {
 				options: {
 					outputStyle: 'nested'
+				},
+				files: {
+					'stylesheets/style.css': 'src/scss/style.scss'
+				}
+			},
+			compressed: {
+				options: {
+					outputStyle: 'compressed'
 				},
 				files: {
 					'stylesheets/style.css': 'src/scss/style.scss'
@@ -47,11 +61,11 @@ module.exports = function (grunt) {
 			}
 		},
 
-		// Watch
+		// Watch files changes
 		watch: {
 			sass: {
 				files: 'src/scss/**/*.scss',
-				tasks: [ 'sass' ],
+				tasks: [ 'sass:nested' ],
 				options: {
 					livereload: true
 				}
@@ -59,6 +73,13 @@ module.exports = function (grunt) {
 			html: {
 				files: 'src/**/*.html',
 				tasks: [ 'includereplace' ],
+				options: {
+					livereload: true
+				}
+			},
+			img: {
+				files: 'src/images/**/*.{png,jpg,gif,svg,ico}',
+				tasks: [ 'copy:images' ],
 				options: {
 					livereload: true
 				}
@@ -80,8 +101,33 @@ module.exports = function (grunt) {
 					base: ''
 				}
 			}
-		}
+		},
 
+		// Image optimization
+		imagemin: {
+			all: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/images/',
+						src: ['**/*.{png,jpg,gif}'],
+						dest: 'images/'
+					}
+				]
+			}
+		},
+
+		// HTML validation
+		validation: {
+			options: {
+				reset: grunt.option('reset') || false,
+				stoponerror: false,
+				relaxerror: ["Bad value X-UA-Compatible for attribute http-equiv on element meta."]
+			},
+			files: {
+				src: ['**/*.html', '!src/**/*.html', '!node_modules/**/*.html']
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
@@ -90,11 +136,17 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-include-replace');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-notify');
+	grunt.loadNpmTasks('grunt-html-validation');
 
-	grunt.registerTask('build', 'Prepare build files', ['clean:build', 'copy:build', 'includereplace', 'sass']);
-	grunt.registerTask('start', 'Starts local server and watch files', ['connect', 'watch-files']);
-	grunt.registerTask('watch-files', 'Watch files', ['watch']);
-	grunt.registerTask('clean-files', 'Clean build files', ['clean:build']);
+	// Tasks
+	grunt.registerTask('default', 'Watch files', ['watch']);
+	grunt.registerTask('start', 'Starts local server and watch files', ['connect', 'default']);
+	grunt.registerTask('validate', 'Validate html files', ['validation']);
+	grunt.registerTask('html', 'Compile html', ['includereplace']);
+	grunt.registerTask('css', 'Compile sass', ['sass']);
+	grunt.registerTask('img', 'Optimize image files', ['imagemin']);
+	grunt.registerTask('build', 'Prepare build files', ['clean:all', 'copy:all', 'includereplace', 'sass:compressed', 'img']);
 
 };
